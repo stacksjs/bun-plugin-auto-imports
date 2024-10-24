be53e384944920315f8233ef6ab8d50c03dcb730
Chris <chrisbreuer93@gmail.com>
2024-10-24 15:41:02 +0200

    chore: initial commit
    chore: wip
    
    chore: wip
    
    chore: wip

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import type { PluginBuilder } from 'bun'
+import type { UnimportOptions } from 'unimport'
+
+function getLoader(path: string) {
+  return path.endsWith('ts')
+    ? 'ts'
+    : path.endsWith('js')
+      ? 'js'
+      : path.endsWith('tsx')
+        ? 'tsx'
+        : 'jsx'
+}
+
+export function autoImports(options: Partial<UnimportOptions & { dts: string }>) {
+  return {
+    name: 'bun-plugin-auto-imports',
+    async setup(builder: PluginBuilder): Promise<void> {
+      const { createUnimport } = await import('unimport')
+      const { injectImports, generateTypeDeclarations } = createUnimport({
+        ...options,
+        dts: undefined,
+      } as UnimportOptions)
+
+      const dtsContent = await generateTypeDeclarations()
+      Bun.write(options.dts ?? './auto-import.d.ts', dtsContent)
+
+      builder.onLoad({ filter: /.*/ }, async (args) => {
+        const fileContent = await Bun.file(args.path).text()
+        const transformedFileContent = await injectImports(fileContent)
+
+        return {
+          contents: transformedFileContent.code,
+          loader: getLoader(args.path),
+        }
+      })
+    },
+  }
+}
+
+export type AutoImportsOptions = UnimportOptions
+
+export default autoImports
